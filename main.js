bcbc5394ecff5fb58e655cad1e426905a7cf5f58
perborgen <perhb@hotmail.com>
2015-11-21 09:21:37 +0100

    stuff

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var Hapi = require('hapi');
-var Joi = require('joi');
+//var Joi = require('joi');
 
 
 var internals = {};
@@ -7,18 +7,18 @@ var internals = {};
 
 // Type shortcuts
 
-var S = Joi.string;
+/*var S = Joi.string;
 var N = Joi.number;
 var A = Joi.array;
 
-
+*/
 
 internals.get = function (request, reply) {
+    reply.file('./public/index.html');
 
-    reply('Success!\n');
 };
 
-
+/*
 internals.output = function (request, reply) {
 
     reply('Success!\n');
@@ -44,7 +44,7 @@ internals.redirect = function (request, reply) {
 internals.error = function (request, reply) {
 
     reply('Success!\n');
-};
+};*/
 
 var server = new Hapi.Server();
 
@@ -53,20 +53,28 @@ server.connection({
     host: '0.0.0.0' 
 });
 
-server.route([
-    { method: 'GET', path: '/', config: { handler: internals.get, validate: { query: { username: S() } } } },
-    { method: 'POST', path: '/', config: { handler: internals.echo, payload: { parse: true } } },
-    { method: 'GET', path: '/admin', config: { handler: internals.get, validate: { query: { username: S().required(), password: S().required() } } } },
-    { method: 'GET', path: '/users', config: { handler: internals.get, validate: { query: { email: S().email().required().min(18) } } } },
-    { method: 'GET', path: '/config', config: { handler: internals.get, validate: { query: { choices: A().required() } } } },
-    { method: 'GET', path: '/test', config: { handler: internals.get, validate: { query: { num: N().min(0) } } } },
-    { method: 'GET', path: '/output', config: { handler: internals.output, validate: { query: { input: S().min(3) } } } },
-    { method: 'GET', path: '/users/{id}', config: { description: 'Get a user', handler: internals.get, validate: { params: { id: N().required() }, query: { name: S().description('the user name').required() } } } },
-    { method: 'GET', path: '/redirect', config: { handler: internals.redirect } },
-    { method: 'GET', path: '/error', config: { handler: internals.error } }
-]);
 
-var schema = Joi.object().keys({
+server.register([require('inert')], function(err) {
+
+    server.route([
+        { 
+            method: 'GET', 
+            path: '/', 
+            config: { handler: internals.get } 
+        }
+    /*    { method: 'POST', path: '/', config: { handler: internals.echo, payload: { parse: true } } },
+        { method: 'GET', path: '/admin', config: { handler: internals.get, validate: { query: { username: S().required(), password: S().required() } } } },
+        { method: 'GET', path: '/users', config: { handler: internals.get, validate: { query: { email: S().email().required().min(18) } } } },
+        { method: 'GET', path: '/config', config: { handler: internals.get, validate: { query: { choices: A().required() } } } },
+        { method: 'GET', path: '/test', config: { handler: internals.get, validate: { query: { num: N().min(0) } } } },
+        { method: 'GET', path: '/output', config: { handler: internals.output, validate: { query: { input: S().min(3) } } } },
+        { method: 'GET', path: '/users/{id}', config: { description: 'Get a user', handler: internals.get, validate: { params: { id: N().required() }, query: { name: S().description('the user name').required() } } } },
+        { method: 'GET', path: '/redirect', config: { handler: internals.redirect } },
+        { method: 'GET', path: '/error', config: { handler: internals.error } }*/
+    ]);
+});
+
+/*var schema = Joi.object().keys({
     title: S().required().invalid('director'),
     status: S().required().valid('open', 'pending', 'close'),
     participants: A().required().includes(S(), N()).min(2)
@@ -81,7 +89,7 @@ server.route({
             payload: schema
         }
     }
-});
+});*/
 
 server.start(function () {
 
