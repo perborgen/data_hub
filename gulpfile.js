ca074f43dd446484f6615fec40ee2b6899ed3d52
perborgen <perhb@hotmail.com>
2015-11-21 10:41:51 +0100

    added gulp to npm start

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var babelify   = require('babelify');
 var browserify = require('browserify');
 var fs         = require('fs');
 var nodemon = require('gulp-nodemon');
+var server = require('./main').server;
 
+/*
 var build = function () {
   console.log("Build started...");
   var timer_name = "Build complete";
@@ -27,3 +29,35 @@ var build = function () {
 gulp.task('build', function () {
   build();
 });
+*/
+
+
+gulp.task('watch', function(){
+  console.log('starting watch');
+	gulp.watch("./app/components/*.js",['build']);
+});
+
+gulp.task('build', function() {
+	console.log('running build task');
+  // From Babel's example setup.
+  // See https://babeljs.io/docs/setup/#browserify
+  browserify({ debug: true })
+    .transform(babelify)
+    .require("./app/client.js", { entry: true })
+    .bundle()
+    .on("error", function (err) { console.log("Error: " + err.message); })
+    .pipe(fs.createWriteStream("./public/bundle.js"));
+});
+
+gulp.task('runServer', function(){
+  console.log('runServer');
+	server.start(function (err) {
+    if (err) {
+          throw err;
+      }
+    console.log('Server running');
+  });
+});
+
+
+gulp.task('default', ['build', 'watch', 'runServer']);
