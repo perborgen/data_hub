ec84c88d94df2e23f912e99b28bf6a10782a442d
perborgen <perhb@hotmail.com>
2015-11-22 14:27:26 +0100

    stuff

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	mongoose: 'mongodb://per:123@ds054298.mongolab.com:54298/datahub',
+	password: 'github-encryption-password',
+    clientId: 'de093188762870bce624',//'YourAppId',
+    clientSecret: 'f16759c338298e4443274b3d3b7497dc701cc5e6'
+};
