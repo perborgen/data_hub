c64270488c56dc372eded3bd1fbd5affa0fd8a4c
perborgen <perhb@hotmail.com>
2016-03-18 19:00:14 +0100

    change from index to main + add stage-0 to babel. application error

--- gulpfile.js
+++ gulpfile.js
@@ -31,25 +31,51 @@ gulp.task('build', function () {
 
 //gulp.task('default', ['build']);
 
-gulp.task('watch', function(){
- // console.log('starting watch');
-  gulp.watch(["./app/components/*/*.js", "./app/components/*.js"], ['build']);
+
+var build = function () {
+  var timer_name = "Build complete";
+  console.time(timer_name);
+  // from babel's example setup.
+  // see https://babeljs.io/docs/setup/#browserify
+  browserify({ debug: true })
+    .transform(babelify, {presets: ["es2015", "react", "stage-0"]})
+    .require("./app/client.js", { entry: true })
+    .bundle()
+    .on("error", function (err) {
+      console.error("error: " + err.message);
+    })
+    .on('end', function() {
+      console.timeEnd(timer_name);
+    })
+    .pipe(fs.createWriteStream("./public/bundle.js"));
+};
+
+gulp.task('build', function () {
+  build();
 });
 
 gulp.task("heroku:production", function(){
     console.log('hello'); // the task does not need to do anything.
 });
 
+
+gulp.task('watch', function(){
+ // console.log('starting watch');
+  gulp.watch(["./app/components/*/*.js", "./app/components/*.js"], ['build']);
+});
+
+
+/*
 gulp.task('build', function() {
   // From Babel's example setup.
   // See https://babeljs.io/docs/setup/#browserify
   browserify({ debug: true })
-    .transform(babelify)
+    .transform(babelify,  {presets: ["es2015", "react"]})
     .require("./app/client.js", { entry: true })
     .bundle()
     .on("error", function (err) { console.log("Error: " + err.message); })
     .pipe(fs.createWriteStream("./public/bundle.js"));
-});
+});*/
 
 /*gulp.task('runServer', function(){
   console.log('runServer');
